41ed4ee670d806531d8b2943ed0ff23cd3c0fbc6
Unknown <JulianHilka@gmx.de>
2017-11-12 13:13:26 +0100

    Irgendwas hier rein

--- ErgoCalculator/ErgoCalculatorUI/MainWindow.xaml.cs
+++ ErgoCalculator/ErgoCalculatorUI/MainWindow.xaml.cs
@@ -24,5 +24,15 @@ namespace ErgoCalculatorUI
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
