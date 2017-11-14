e50a12a1fd1d3662088816b940f5e92dba3b7153
Tobias Schaub <schaubi93@gmail.com>
2017-11-14 19:56:30 +0100

    Alles Zusammen
    Weil ich ein fauler Sack bin

--- ErgoCalculator/ErgoCalculatorUI/MainWindow.xaml.cs
+++ ErgoCalculator/ErgoCalculatorUI/MainWindow.xaml.cs
@@ -12,15 +12,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ErgoCalculatorUI.Calculator;
 
 namespace ErgoCalculatorUI
 {
+
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
         List<String> calculation = new List<String>();
+        AngleMode currentAngleMode = AngleMode.RAD;
+        ShuntingYardParser parser = new ShuntingYardParser();
 
         public MainWindow()
         {
@@ -30,18 +35,25 @@ namespace ErgoCalculatorUI
             {
                 btn.Click += Btn_Click;
             }
+            btnDEL.Click -= Btn_Click;
+            btnAC.Click -= Btn_Click;
+            btnEquals.Click -= Btn_Click;
+
+            btnDegRad.Content = Enum.GetName(typeof(AngleMode), currentAngleMode);
         }
 
         private void Btn_Click(object sender, RoutedEventArgs e)
         {
-            //Console.WriteLine(((Button)sender).Content);
             calculation.Add(((Button)sender).Content.ToString());
             UpdateCalculationText();
         }
 
         private void btnDegRad_Click(object sender, RoutedEventArgs e)
         {
-            ((Button)sender).Content = "RAD";
+            int nextMode = ((int)currentAngleMode + 1) % Enum.GetValues(typeof(AngleMode)).Length;
+            currentAngleMode = (AngleMode)nextMode;
+            Enum.GetName(typeof(AngleMode), currentAngleMode);
+            btnDegRad.Content = Enum.GetName(typeof(AngleMode), currentAngleMode);
         }
 
         private void btnSettings_Click(object sender, RoutedEventArgs e)
@@ -52,6 +64,35 @@ namespace ErgoCalculatorUI
         private void btnEquals_Click(object sender, RoutedEventArgs e)
         {
             // Parse String to calculate
+            List<string> preParse = new List<string>();
+            int pos = 0;
+            int output;
+            foreach (var token in calculation)
+            {
+                if (Int32.TryParse(token, out output))
+                {
+                    if (preParse.Count == pos) preParse.Add(token);
+                    else preParse[pos] += token;
+                }
+                else if (token == ",")
+                {
+                    preParse[pos] += ".";
+                }
+                else
+                {
+                    preParse.Add(token);
+                    pos = preParse.Count;
+                }
+            }
+
+            string[] parsedOutput = parser.Parse(preParse.ToArray());
+            decimal result = RPNEvaluator.Evaluate(string.Join(" ", parsedOutput));
+
+            // TODO: save in history
+            calculation.Clear();
+            calculation.Add(result.ToString());
+            UpdateCalculationText();
+
         }
 
         private void btnAC_Click(object sender, RoutedEventArgs e)
@@ -64,14 +105,21 @@ namespace ErgoCalculatorUI
         private void btnDEL_Click(object sender, RoutedEventArgs e)
         {
             // Delete last Operation
+            if (calculation.Count == 0)
+                return;
+
             calculation.RemoveAt(calculation.Count - 1);
+            UpdateCalculationText();
         }
 
         private void UpdateCalculationText()
         {
             StringBuilder sb = new StringBuilder();
 
-            calculation.ForEach(x => sb.Append(x));
+            if (calculation.Count == 0)
+                sb.Append(0);
+            else
+                calculation.ForEach(x => sb.Append(x));
 
             txtCalculation.Text = sb.ToString();
         }
